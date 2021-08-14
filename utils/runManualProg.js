3d0304fb59d974527c13ca8b4e2fbb6e1313d3a9
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-08-14 14:43:30 +0800

    juicebox & opensprinkler running from create fert

--- utils/runManualProg.js
+++ utils/runManualProg.js
@@ -11,19 +11,14 @@ const stationConverter = (stnCount, stationNum, time) => {
   // build array
   const runArray = new Array(stnCount).fill(0);
   runArray.fill(Number(time), Number(stationNum) - 1, Number(stationNum));
-  // for (let i = 0; i < stnCount; i++) {
-  //   if (i + 1 === Number(stationNum)) {
-  //     runArray.push(Number(time));
-  //   } else {
-  //     runArray.push(0);
-  //   }
-  // }
   return runArray;
 };
 
-const runManualProgram = ({ property, stationNumber, fertRuntime }) => {
-  console.log('in RMP', property.zones.length);
-  console.log(stationConverter(16, stationNumber, fertRuntime));
+const runManualProgram = async ({ property, stationNumber, fertRuntime }) => {
+  // console.log('in RMP', property.zones.length);
+  // console.log(
+  //   stationConverter(property.zones.length, stationNumber, fertRuntime)
+  // );
 
   // change master valve to juicebox
   // setup mqtt client
@@ -46,7 +41,21 @@ const runManualProgram = ({ property, stationNumber, fertRuntime }) => {
   // switch the valve to juicebox
   mqttClient.publish(`${property.juiceBoxId}/relay1`, 'on');
   // start opensprinkler
-
+  try {
+    const res = await axios.get(`${property.openSprinklerAddress}cr`, {
+      params: {
+        pw: property.openSprinklerKey,
+        t: `${stationConverter(
+          property.zones.length,
+          stationNumber,
+          fertRuntime
+        )}`,
+      },
+    });
+    console.log(res);
+  } catch (error) {
+    console.error(error);
+  }
   // set turn off after set time.
   setTimeout(() => {
     mqttClient.publish(`${property.juiceBoxId}/relay1`, 'off');
