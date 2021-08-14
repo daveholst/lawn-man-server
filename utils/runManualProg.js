90103e4c27df83d5ffb032623588da17560a6a1a
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-08-14 14:23:51 +0800

    station run array working

--- utils/runManualProg.js
+++ utils/runManualProg.js
@@ -7,8 +7,24 @@
 const axios = require('axios').default;
 const mqtt = require('mqtt');
 
-const runManualProgram = (args) => {
-  console.log('in RMP', args);
+const stationConverter = (stnCount, stationNum, time) => {
+  // build array
+  const runArray = new Array(stnCount).fill(0);
+  runArray.fill(Number(time), Number(stationNum) - 1, Number(stationNum));
+  // for (let i = 0; i < stnCount; i++) {
+  //   if (i + 1 === Number(stationNum)) {
+  //     runArray.push(Number(time));
+  //   } else {
+  //     runArray.push(0);
+  //   }
+  // }
+  return runArray;
+};
+
+const runManualProgram = ({ property, stationNumber, fertRuntime }) => {
+  console.log('in RMP', property.zones.length);
+  console.log(stationConverter(16, stationNumber, fertRuntime));
+
   // change master valve to juicebox
   // setup mqtt client
   const mqttClient = mqtt.connect(
@@ -20,20 +36,21 @@ const runManualProgram = (args) => {
   );
 
   mqttClient.on('connect', () => {
-    mqttClient.subscribe(`${args.juiceBoxId}/#`, (err) => {
+    mqttClient.subscribe(`${property.juiceBoxId}/#`, (err) => {
       if (!err) {
-        mqttClient.publish(`${args.juiceBoxId}`, 'Server Online!');
+        mqttClient.publish(`${property.juiceBoxId}`, 'Server Online!');
       }
     });
   });
 
   // switch the valve to juicebox
-  mqttClient.publish(`${args.juiceBoxId}/relay1`, 'on');
+  mqttClient.publish(`${property.juiceBoxId}/relay1`, 'on');
+  // start opensprinkler
 
   // set turn off after set time.
   setTimeout(() => {
-    mqttClient.publish(`${args.juiceBoxId}/relay1`, 'off');
-  }, args.fertRuntime * 1000);
+    mqttClient.publish(`${property.juiceBoxId}/relay1`, 'off');
+  }, fertRuntime * 1000);
   // switch the valve back
 };
 
